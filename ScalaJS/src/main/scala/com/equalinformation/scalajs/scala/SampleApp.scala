8c6125ce90856e553c45a3f19f797fbd20942e8d
bpupadhyaya <bpupadhyaya@gmail.com>
2016-06-26 21:56:37 -0700

    ScalaJS - JQuery based implementation.

--- ScalaJS/src/main/scala/com/equalinformation/scalajs/scala/SampleApp.scala
+++ ScalaJS/src/main/scala/com/equalinformation/scalajs/scala/SampleApp.scala
@@ -1,16 +1,31 @@
 package com.equalinformation.scalajs.scala
 
-import org.scalajs.dom
-import dom.document
+
+import org.scalajs.jquery.jQuery
 
 import scala.scalajs.js.JSApp
-import scala.scalajs.js.annotation.JSExport
 
 /**
   * Created by bpupadhyaya on 6/18/16.
   */
 object SampleApp extends JSApp {
-  def main(): Unit = {
+
+    def main(): Unit = {
+      jQuery(setupUI _)
+    }
+
+  def setupUI(): Unit = {
+    jQuery("#click-button").click(addButtonClickMessage _)
+    jQuery("body").append("<p>Test program jQuery</p>")
+  }
+
+    def addButtonClickMessage(): Unit = {
+     jQuery("body").append("<p>Test program button click jQuery </p>")
+    }
+
+
+  // DOM based implementation - uncomment if you want to test DOM based solution, but jQuery uses DOM internally so...
+/*  def main(): Unit = {
     appendPar(document.body, "Test program")
   }
 
@@ -25,6 +40,6 @@ object SampleApp extends JSApp {
   @JSExport
   def addButtonClickMessage(): Unit = {
     appendPar(document.body, "Test program button click")
-  }
+  }*/
 
 }
